7084f64e60691f1ec300033ef61a8e353586f746
muhammadumer811 <muhammmadumer8110@gmail.com>
2025-08-03 15:50:56 +0500

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,191 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+
+namespace practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter number of rows:");
+            //int rows = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= rows; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("ggal ko samjh");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.ReadKey();
+
+
+
+            //Console.WriteLine("Choose A Shape Circle / Triangle ");
+            //string shape = Convert.ToString(Console.ReadLine());
+
+            //double area = 0;
+
+            //if (shape == "Circle")
+            //{
+            //    Console.WriteLine("Enter a Circle");
+            //    double radius = Convert.ToDouble(Console.ReadLine());
+            //    area = Math.PI * radius * radius;
+            //}
+            //else if (shape == "Triangle")
+            //{
+            //    Console.WriteLine("Enter a Base");
+            //    double baselength = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.WriteLine("Enter a Length");
+            //    double height = Convert.ToDouble(Console.ReadLine());
+
+            //    area = 0.5 * baselength * height;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid shape");
+            //}
+            //Console.WriteLine($"Result {area}");
+            //Console.ReadKey();
+
+            //Console.WriteLine("Enter a number 1 tp 3");
+            //int choice = Convert.ToInt32(Console.ReadLine());
+
+            //switch (choice)
+            //{
+            //    case 1:
+            //        Console.WriteLine("you selected one");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("you selected two");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("you selected three");
+            //        break;
+            //    default:
+            //        Console.WriteLine("invalid choice");
+            //        break;
+            //}
+
+
+            //Console.WriteLine("enter a number for multiplication");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //for ( int i=1; i<=10; i++)
+            //{
+            //    Console.WriteLine($"{num} x  {i} = {num * i}");
+
+            //}
+
+            //write a program to increase a salary of an empolyee form 500 if the entered salary is less than 10000 otherwise increase by 800
+
+
+            //Console.WriteLine("enter a salary");
+            //double salary = Convert.ToDouble(Console.ReadLine());
+
+
+            //if (salary < 10000)
+            //{
+            //    salary = salary + 500;
+            //    Console.WriteLine("salary is less than 10000, added 500 = " + salary);
+            //}
+            //else if (salary > 10000)
+            //{
+            //    salary = salary + 800;
+            //    Console.WriteLine("salary is greater than 10000, added 800 = " + salary);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("the actual salary is " + salary);
+            //}
+
+
+            //Console.WriteLine("enter a number");
+            //double year = Convert.ToDouble(Console.ReadLine());
+
+            //if (year % 400 == 0) 
+            //{
+            //    Console.WriteLine("a leap year");
+            //}
+            //else if ( year % 4 == 0)
+            //{
+            //    Console.WriteLine("a leap year");
+            //}
+            //else if( year % 100 != 0)
+            //{
+            //    Console.WriteLine("not a leap year");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not a leap year");
+            //}
+
+            //Console.WriteLine("choose one letter y or n? ");
+            //char letter = Convert.ToChar(Console.ReadLine());
+
+            //if ( letter == 'y')
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+
+            //string correctusername = "muhammad";
+            //string correctpassword = "umer";
+
+            //Console.Write("enter a username: ");
+            //string username = Convert.ToString(Console.ReadLine());
+
+            //Console.Write("enter a password: ");
+            //string password = Convert.ToString(Console.ReadLine());
+
+            //if (username == correctusername && password == correctpassword)
+            //{
+            //    Console.WriteLine("login successfull");
+            //}
+            //else
+            //{
+            //    if (username != correctusername)
+            //    {
+            //        Console.WriteLine("invalid username");
+            //    }
+
+            //    if (password != correctpassword)
+            //    {
+            //        Console.WriteLine("invalid password");
+            //    }
+            //}
+
+
+              //nestedloop 
+
+
+            //for ( int i = 1; i <= 5; i++)
+            //{
+            //    for( int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(i);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for ( int i = 5; i >= 1; i--)
+            //{
+            //    for ( int j = 1; j<= i; j++)
+            //    {
+            //        Console.Write(j % 2);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            
+        } 
+
+    }
+}
